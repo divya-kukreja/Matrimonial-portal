32086f2ed8f928d81faa3ebdf64c9e06a4f15771
Parth Kalbag <10bparth@gmail.com>
2020-12-08 16:56:46 +0530

    Added login to authority_login.php in admin and authority

--- admin/authority_login.php
+++ admin/authority_login.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
    
     <link rel="stylesheet" href="css/login.css">
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> 
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+      <link rel="icon" href="../images/favicon.ico" type="image/ico">
   </head>
   <?php
     require_once "mysql_connection.php";
--- authority_team/authority_login.php
+++ authority_team/authority_login.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/login.css">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-      <link rel="icon" type="image/ico" href="">
+      <link rel="icon" type="image/ico" href="../images/favicon.ico">
       <title>Authority Login</title>
   </head>
   <?php
